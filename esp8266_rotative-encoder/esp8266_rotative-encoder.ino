85eb86b6595f04c8b8c3d0d77c7443a6c5958d33
pierrebressy <pierre.bressy@heig-vd.ch>
2022-04-23 23:22:34 +0200

    Update esp8266_rotative-encoder.ino
    dlam

--- esp8266_rotative-encoder/esp8266_rotative-encoder.ino
+++ esp8266_rotative-encoder/esp8266_rotative-encoder.ino
@@ -5,6 +5,7 @@ int pushButtonPin = 2;    // GPIO2, pin D4
 
 volatile int encoderPos = 0;
 volatile int pushButtonCount = 0;
+char oldDataPinState=0;
 
 unsigned int loopCounter = 0;
 
@@ -12,12 +13,12 @@ ICACHE_RAM_ATTR void clkInterrupt()
 {
     encoderPos += digitalRead(dataPin) == HIGH ? 1 : -1;
 }
-
+#if 0
 ICACHE_RAM_ATTR void pushButtonInterrupt()
 {
     pushButtonCount++;
 }
-
+#endif
 void setup()
 {
 
@@ -28,11 +29,20 @@ void setup()
     pinMode(pushButtonPin, INPUT);
     
     attachInterrupt(digitalPinToInterrupt(clkInterruptPin), clkInterrupt, RISING);
-    attachInterrupt(digitalPinToInterrupt(pushButtonPin), pushButtonInterrupt, RISING);
+    //attachInterrupt(digitalPinToInterrupt(pushButtonPin), pushButtonInterrupt, RISING);
+    
+    oldDataPinState=digitalRead(dataPin);
 }
 
 void loop()
 {
+    if (oldDataPinState!=digitalRead(dataPin)) {
+        oldDataPinState=digitalRead(dataPin);
+        pushButtonCount++;
+        delayMicroseconds(100000);
+    }
+           
+
     Serial.print(loopCounter);
     Serial.print(" encoderPos:");
     Serial.print(encoderPos, DEC);
